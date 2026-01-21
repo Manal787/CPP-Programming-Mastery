c91fd2a441642669eca433e150b105f4fd9aa106
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-21 16:24:44 +0300

    Create Problem-06.cpp
    Solution for Problem#06 in the C++ series by Eng. Mohammed Abo - Hadhoud.

---
+++ Problem-06.cpp
@@ -0,0 +1,38 @@
+struct stInfo 
+{ 
+ string FirstName; 
+ string LastName; 
+}; 
+ 
+stInfo ReadInfo() 
+{ 
+ stInfo Info; 
+ cout << "Please enter your First Name?" << endl; 
+ cin >> Info.FirstName; 
+ 
+ cout << "Please enter your Last Name?" << endl; 
+ cin >> Info.LastName; 
+ 
+ return Info; 
+ 
+} 
+ 
+string GetFullName(stInfo Info) 
+{ 
+ string FullName = ""; 
+ FullName = Info.FirstName + " " + Info.LastName; 
+ return FullName; 
+ 
+} 
+ 
+void PrintFullName(string FullName) 
+{ 
+ cout << "\n Your full name is :" << FullName << endl; 
+ 
+} 
+ 
+ 
+int main() 
+{ 
+ PrintFullName(GetFullName(ReadInfo())); 
+};
