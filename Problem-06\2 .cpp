98f5bfa1eb657895ba033b1890d04e6209249da1
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-22 21:03:17 +0300

    Create Problem-06\2 .cpp
    Implement a name reader with an option to reverse the full name output

---
+++ Problem-06\2 .cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+
+struct stInfo
+{
+	string FirstName;
+	string LastName;
+};
+
+stInfo ReadInfo()
+{
+	stInfo Info;
+	cout << "Please enter your First Name?" << endl;
+	cin >> Info.FirstName;
+
+	cout << "Please enter your Last Name?" << endl;
+	cin >> Info.LastName;
+
+	return Info;
+
+}
+
+string GetFullName(stInfo Info, bool Reversed)
+{
+	string FullName = "";
+	if (Reversed)
+		FullName = Info.LastName + " " + Info.FirstName;
+	else
+		FullName = Info.FirstName + " " + Info.LastName;
+
+	return FullName;
+
+}
+
+void PrintFullName(string FullName)
+{
+	cout << "\n Your full name is :" << FullName << endl;
+
+}
+
+
+int main()
+{
+	PrintFullName(GetFullName(ReadInfo(), true));
+};
+
