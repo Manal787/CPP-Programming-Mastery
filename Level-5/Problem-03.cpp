d5fe124c397320cb64bfb8823ff70ec91c213be1
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-22 23:26:06 +0300

    Create Problem-03.cpp
    Solution for Problem #03 in Level 5 - Algorithms & Problem Solving by Eng. Mohammed Abo-Hadhoud

---
+++ Level-5/Problem-03.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int ReadPositiveNumbwe(string Message)
+{
+	int Number = 0;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+
+bool isPerfectNumber(int Number)
+{
+	int Sum = 0;
+	for (int i = 1; i < Number ; i++)
+	{
+		if (Number%i==0)
+		
+			Sum += i;
+	}
+	return Number == Sum;
+
+}
+void PrintResults(int Number)
+{
+	if (isPerfectNumber(Number))
+		cout << Number << " Is Perfect Number.\n";
+	else
+		cout << Number << " Is NOT Perfact Number.\n";
+}
+
+int main()
+{
+	PrintResults(ReadPositiveNumbwe("Please Enter a Positive Number ? "));
+	return 0;
+};
