a0d6c8f935b892f1f34711c6b7f3b4df685426af
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-23 19:52:30 +0300

    Create Problem-07.cpp
    This solution is part of the "Algorithms & Problem Solving Level 2" course by Eng.Mohammed Abu - Hadhoud.

---
+++ Level-5/Problem-07.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadPositiveNumber(string Message) {
+    int Number = 0;
+    do {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+
+int ReverseNumber(int Number)
+{
+    int Number2 = 0;
+    int Remainder = 0;
+
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        Number2 = Number2 * 10 + Remainder;
+    }
+    return Number2;
+}
+
+int main()
+{
+    cout << "\n Reverse is :\n"
+        <<ReverseNumber(ReadPositiveNumber("Please Enter a Positive Number?")) << "\n";
+    return 0;
+}
+
