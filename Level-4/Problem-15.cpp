646cb431cb2b47bf82ea565ea85c4e9f20883be0
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-26 22:25:52 +0300

    Create Problem-15.cpp

---
+++ Level-4/Problem-15.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+void ReadNumbers(float& A, float& B)
+{
+	cout << "Please enter rectangle width A?" << endl;
+	cin >> A;
+	cout << "Please enter  rectangle length B? " << endl;
+	cin >> B;
+}
+float CalculateRectangleArea(float A, float B)
+{
+	return A * B;
+}
+void PrintResult(float Area)
+{
+	cout << "\n Rectangle Area = " << Area << endl;
+}
+
+int main()
+{
+	float A, B;
+	ReadNumbers(A, B);
+	PrintResult(CalculateRectangleArea(A, B));
+	return 0;
+};
