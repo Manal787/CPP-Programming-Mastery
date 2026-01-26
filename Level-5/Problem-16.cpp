0b79109b82a4e18534334028c28680c7f843f55a
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-26 22:59:50 +0300

    Create Problem-16.cpp

---
+++ Level-5/Problem-16.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void PrintWordsFromAAAtoZZZ()
+{
+    cout << endl;
+    string word = "";
+
+    for (int i = 65; i <= 90; i++)
+    {
+        for (int j = 65; j <= 90; j++)
+        {
+            for (int k = 65; k <= 90; k++)
+            {
+                word = word + char(i);
+                word = word + char(j);
+                word = word + char(k);
+
+                cout << word << endl;
+
+                word = "";
+
+            }
+        }
+        cout << "\n___________________________________\n";
+    }
+}
+
+int main()
+{
+    PrintWordsFromAAAtoZZZ();
+
+    return 0;
+}
+
