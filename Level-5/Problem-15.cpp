7e313aa9ca20f83798cc66bf3ea2bb424cdc9e68
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-26 22:46:45 +0300

    Create Problem-15.cpp

---
+++ Level-5/Problem-15.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadPositiveNumber(string Message) {
+    int Number = 0;
+    do {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+
+void PrintLetterPattern(int Number)
+{
+    cout << endl;
+
+    for (int i = 65; i <= 65+ Number-1; i++)
+    {
+        for (int j = 1; j <= i -65+1; j++)
+        {
+            cout << char(i);
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    PrintLetterPattern(ReadPositiveNumber
+    ("Please Enter a positive Number?"));
+
+    return 0;
+}
