5e9fe48ca7f6cc9520d2e87bfad9ec769ec988b5
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-27 19:18:06 +0300

    Create Problem-18.cpp

---
+++ Level-4/Problem-18.cpp
@@ -0,0 +1,26 @@
+float ReadRadious() 
+{ 
+ float R; 
+ 
+ cout << "Please enter Radious R?" << endl; 
+ cin >> R; 
+ 
+ return R; 
+} 
+float CircleArea(float R) 
+{ 
+ const float PI = 3.141592653589793238; 
+ float Area = pow(R, 2) * PI; 
+ return Area; 
+} 
+void PrintResult(float Area ) 
+{ 
+ cout << "\n Circle Area = " << Area << endl; 
+} 
+ 
+int main() 
+{ 
+  
+ PrintResult(CircleArea(ReadRadious())); 
+ return 0; 
+};
