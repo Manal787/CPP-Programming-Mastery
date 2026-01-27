5b9b20bbc71748084e557410f4ce9273d816cd0d
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-27 20:31:55 +0300

    Create Problem-19.cpp

---
+++ Level-5/Problem-19.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    cout << RandomNumber(20, 50) << endl;
+    cout << RandomNumber(20, 50) << endl;
+    cout << RandomNumber(20, 50) << endl;
+
+
+    return 0;
+}
