fe55c1086c5bd94770ef8222a39076e2a014e4f9
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-28 21:18:54 +0300

    Create Problem-20.cpp

---
+++ Level-4/Problem-20.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+
+float ReadSquareSide()
+{
+	float A;
+
+	cout << "Please enter Square side A?" << endl;
+	cin >> A;
+
+	return A;
+}
+float CircleAreaInscribedTnSquare(float A)
+{
+	const float PI = 3.141592653589793238;
+
+	float Area = (PI * pow(A, 2)) / 4;
+
+	return Area;
+}
+void PrintResult(float Area)
+{
+	cout << "\n Circle Area = " << Area << endl;
+}
+
+int main()
+{
+	PrintResult(CircleAreaInscribedTnSquare(ReadSquareSide()));
+	return 0;
+};
