ed7fd9a17974e29cb8890c6d97eb6f7f2cb4ba64
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-31 17:42:15 +0300

    Create Problem-28.cpp

---
+++ Level-5/Problem-28.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+void FillArrayWithRandomNumber(int arr[100], int& arrLength)
+{
+    cout << "\nEnter Number Of Elements:\n";
+    cin >> arrLength;
+
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+void CopyArray(int arrSource[100],int arrDestination[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+        arrDestination[i] = arrSource[i];
+}
+int main()
+{
+    srand((unsigned)time(NULL)); 
+
+    int arr[100],arrLength;
+
+    FillArrayWithRandomNumber(arr, arrLength);
+
+    int arr2[100];
+    CopyArray(arr, arr2, arrLength);
+
+    cout << "\nArray 1 Elements:";
+    PrintArray(arr, arrLength);
+
+    cout << "\nArray 2 Elements After Copy :\n ";
+    PrintArray(arr, arrLength);
+    
+
+    return 0;
+}
