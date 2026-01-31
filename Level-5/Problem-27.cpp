a090cef1c817e501eb86045c52072b163a7053dc
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-01-31 17:21:56 +0300

    Create Problem-27.cpp

---
+++ Level-5/Problem-27.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+void FillArrayWithRandomNumber(int arr[100], int& arrLength)
+{
+    cout << "\nEnter Number Of Elements:\n";
+    cin >> arrLength;
+
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+int SumArray(int arr[100],int arrLength)
+{
+    int Sum = 0;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        Sum += arr[i];
+    }
+    return Sum;
+}
+float ArrayAverage(int arr[100], int arrLength)
+{
+    return(float)SumArray(arr, arrLength) / arrLength;
+}
+int main()
+{
+    srand((unsigned)time(NULL)); 
+
+    int arr[100], arrLength;
+
+    FillArrayWithRandomNumber(arr, arrLength);
+
+    cout << "\nArray Elements:";
+    PrintArray(arr, arrLength);
+
+    cout << "\nAverage Of All Number is : ";
+    cout << ArrayAverage(arr, arrLength) << endl;
+    
+
+    return 0;
+}
