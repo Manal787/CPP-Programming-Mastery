470f2882273496870f451396beb514e96e21e8ca
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-02-05 08:50:20 +0300

    Create Problem-38.cpp

---
+++ Level-5/Problem-38.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumber(int arr[100], int& arrLength)
+{
+    cout << "\nEnter Number Of Elements:\n";
+    cin >> arrLength;
+
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void AddArrayElement(int Number,int arr[100], int& arrLength)
+{
+    arrLength++;
+    arr[arrLength-1] = Number;
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+    cout << "\n";
+}
+
+void CopyOddNumbers(int arrSource[100], int arrDestination[100], int arrLength, int& arrDestinationLength)
+{
+    for (int  i = 0; i < arrLength; i++)
+        if (arrSource[i] % 2 != 0)
+    {
+        AddArrayElement(arrSource[i], arrDestination, arrDestinationLength);
+    }
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength=0;
+    int arr2Length = 0;
+
+    FillArrayWithRandomNumber(arr, arrLength);
+    
+    int arr2[100];
+    CopyOddNumbers(arr, arr2, arrLength, arr2Length);
+
+    cout << "Array 1 Elements:\n";
+    PrintArray(arr, arrLength);
+
+    cout << "Array 2 Odd Numbers:\n";
+    PrintArray(arr2, arr2Length);
+    return 0;
+}
