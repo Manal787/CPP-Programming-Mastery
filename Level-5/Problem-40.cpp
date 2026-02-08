d4b50746ad4ce200671af2e441c300e55522597a
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-02-08 20:20:30 +0300

    Create Problem-40.cpp

---
+++ Level-5/Problem-40.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
+using namespace std;
+
+void FillArray(int arr[100], int& arrLength)
+{
+    arrLength = 10;
+    arr[0] = 10;
+    arr[1] = 10;
+    arr[2] = 10;
+    arr[3] = 50;
+    arr[4] = 50;
+    arr[5] = 70;
+    arr[6] = 70;
+    arr[7] = 70;
+    arr[8] = 70;
+    arr[9] = 90;
+
+}
+
+short FindNumberPositionInArray(int Number, int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i] == Number)
+            return i;  
+    }
+    return -1;
+}
+
+bool IsNumberInArray(int Number, int arr[100], int arrLength)
+{
+    return FindNumberPositionInArray(Number, arr, arrLength) != -1;
+}
+
+void AddArrayElement(int Number,int arr[100], int& arrLength)
+{
+    arrLength++;
+    arr[arrLength-1] = Number;
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+    cout << "\n";
+}
+
+void CopyDistinctNumbersToArray(int arrSource[100],int SourceLength, int arrDestination[100],int&DestinationLength )
+
+{
+    for (int i = 0; i < SourceLength; i++)
+    {
+        if (!IsNumberInArray(arrSource[i], arrDestination, DestinationLength))
+        {
+            AddArrayElement(arrSource[i], arrDestination, DestinationLength);
+
+        }
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arrSource[100], SourceLength = 0, arrDestination[100], DestinationLength = 0;
+    FillArray(arrSource,SourceLength);
+    
+    cout << "Array 1 elements:\n";
+    PrintArray(arrSource,SourceLength);
+
+    CopyDistinctNumbersToArray(arrSource,SourceLength, arrDestination ,DestinationLength);
+
+    cout << "Array 2 distinct elements:\n";
+    PrintArray(arrDestination, DestinationLength);
+    return 0;
+}
