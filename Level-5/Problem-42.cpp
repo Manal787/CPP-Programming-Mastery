26b565d55f2adcb170298694cab51a3f74559e7a
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-02-09 20:12:58 +0300

    Create Problem-42.cpp

---
+++ Level-5/Problem-42.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumber(int arr[100], int& arrLength)
+{
+    cout << "\nEnter Number Of Elements:\n";
+    cin >> arrLength;
+
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+    cout << "\n";
+}
+
+int OddCount(int arr[100], int arrLength)
+{
+    int Counter = 0;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i]%2!=0)
+        {
+            Counter++;
+        }
+
+    }
+    return Counter;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100],arrLength;
+
+    FillArrayWithRandomNumber(arr,arrLength);
+    
+    cout << "Array elements:\n";
+    PrintArray(arr,arrLength);
+
+    cout << "\nOdd Numbers count is:";
+    cout << OddCount(arr, arrLength) << endl;
+    return 0;
+}
