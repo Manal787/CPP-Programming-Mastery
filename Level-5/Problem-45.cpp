9813d9fd2dfd0e0799c200c1377b87d816952f15
Manal787 <143595582+Manal787@users.noreply.github.com>
2026-02-18 22:38:15 +0300

    Create Problem-45.cpp

---
+++ Level-5/Problem-45.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include<cstdlib>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumber(int arr[100], int& arrLength)
+{
+    cout << "\nEnter Number Of Elements:\n";
+    cin >> arrLength;
+
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = RandomNumber(-100, 100);
+    }
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+    cout << "\n";
+}
+
+int NegativeCount(int arr[100],int arrLength)
+{
+    int Counter = 0;
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i]< 0)
+        {
+            Counter++;
+        }
+    }
+    return Counter;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100],arrLength;
+
+    FillArrayWithRandomNumber(arr,arrLength);
+    
+    cout << "Array elements:\n";
+    PrintArray(arr,arrLength);
+
+    cout << "\Negative Numbers count is:";
+    cout << NegativeCount(arr, arrLength) << endl;
+    return 0;
+}
